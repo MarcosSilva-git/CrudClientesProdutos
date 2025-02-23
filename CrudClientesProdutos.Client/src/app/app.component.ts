4eed11d5efa1b0255e36042895f3d1e383c1556b
Marcos Silva <marcosjoseoliveira.mj@gmail.com>
2025-02-23 19:13:32 -0300

    Feat: Adding product Route

--- CrudClientesProdutos.Client/src/app/app.component.ts
+++ CrudClientesProdutos.Client/src/app/app.component.ts
@@ -1,39 +1,12 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-  name: string;
-  //temperatureC: number;
-  //temperatureF: number;
-  //summary: string;
-}
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  imports: [HttpClientModule],
+  template: '<router-outlet></router-outlet>',
+  imports: [RouterModule],
   standalone: true,
-  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('https://localhost:7163/api/v1/Client').subscribe(
-      result => {
-        this.forecasts = result;
-      },
-      error => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'crudclientesprodutos.client';
+export class AppComponent {
+   
 }
