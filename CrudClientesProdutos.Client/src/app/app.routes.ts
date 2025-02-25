95ea5ed67874f65486e84f748b23ad82cb41a4ff
Marcos Silva <marcosjoseoliveira.mj@gmail.com>
2025-02-25 16:08:57 -0300

    feat: navBar

--- CrudClientesProdutos.Client/src/app/app.routes.ts
+++ CrudClientesProdutos.Client/src/app/app.routes.ts
@@ -1,32 +1,18 @@
-import { Route, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { MainLayout } from './layouts/main/main.layout';
-import { PRODUCT_ROUTES } from './features/product/product.routes';
-
-// export const routes: Routes = [
-//     { path: '', redirectTo: '/products', pathMatch: 'full' },
-//     {
-//         path: 'products',
-//         loadChildren: () => import('./features/product/product.routes').then(m => m.PRODUCT_ROUTES),
-//     },
-//     {
-//         path: 'clients',
-//         loadChildren: () => import('./features/client/client.routes').then(m => m.CLIENT_ROUTES),
-//     },
-// ];
-
 
 export const routes: Routes = [
     {
         path: '',
         component: MainLayout,
         children: [
-            { path: '', redirectTo: '/products', pathMatch: 'full' },
+            { path: '', redirectTo: '/produtos', pathMatch: 'full' },
             {
-                path: 'products',
+                path: 'produtos',
                 loadChildren: () => import('./features/product/product.routes').then(m => m.PRODUCT_ROUTES),
             },
             {
-                path: 'clients',
+                path: 'clientes',
                 loadChildren: () => import('./features/client/client.routes').then(m => m.CLIENT_ROUTES),
             },
         ]
--- CrudClientesProdutos.Client/src/app/layouts/main/main.layout.ts
+++ CrudClientesProdutos.Client/src/app/layouts/main/main.layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-main-layout',
-  imports: [MatToolbarModule, RouterModule],
+  imports: [MatToolbarModule, RouterModule, MatButtonModule],
   standalone: true,
   templateUrl: './main.layout.html',
   styleUrl: './main.layout.css'
